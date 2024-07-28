fb7eb25ea22c8a1589ff3719b87eb75f49d092e1
Bruna Vicente <brutronica931@gmail.com>
2024-07-27 22:47:21 -0300

    Aprendendo a mexer com React

---
+++ src/pages/contato/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export default function Contato() {
+    return (
+        <div className='pagina-contato'>
+            <h1>Contato</h1>
+            <img src="/assets/icone_contato-1.png"/>
+        </div>
+    )
+}
